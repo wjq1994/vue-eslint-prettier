243ad425c65bac02f7833f5bbeff9d98e725b921
wjq1994 <1194497392@qq.com>
2021-01-22 20:33:26 +0800

    新增

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,5 +35,7 @@ module.exports = {
 		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"@typescript-eslint/no-explicit-any": "off",
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": ["error"],
 	},
 };
